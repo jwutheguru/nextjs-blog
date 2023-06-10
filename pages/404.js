bdf6bf22468e7d57ea4d10bca4471592e064e30a
Jianzhuo Wu <jwu@jMacBook-Pro-16-M1-Max.local>
2023-06-10 18:36:42 +0900

    Initial commit for nextjs-blog tutorial

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Custom404() {
+  return (
+    <h1>
+      <Head>
+        <title>404 Yo</title>
+      </Head>
+      404 - Your page could not be found.
+    </h1>
+  );
+}
